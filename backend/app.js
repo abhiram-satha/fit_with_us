d56bcc77c28030661a98d51c7755a9b259740785
Brian Duong <brianminhduong@gmail.com>
2022-05-03 01:18:59 -0400

    Add API request to React app

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,26 @@
-const express = require('express')
-const app = express()
-const path = require('path');
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const axios = require('axios')
-const router = express.Router()
-const port = 8080
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const axios = require("axios");
+const router = express.Router();
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 
+app.get("/", (req, res) => {
+  axios
+    .get(
+      "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=d44a082f&app_key=35468e3059752f205fc55cbd181c94bc&calories=100-500"
+    )
+    .then((response) => res.send(response.data));
+});
 
-app.get('/', (req, res)=> {
-  axios.get("https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=d44a082f&app_key=35468e3059752f205fc55cbd181c94bc")
-    .then(response => res.send(response.data))
-})
-
-app.listen(port, ()=> {
-  console.log(`Listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
