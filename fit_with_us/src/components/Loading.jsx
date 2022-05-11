fa8a4b1fa6cffb48ef6f1e13862b829986ac40c8
Brian Duong <brianminhduong@gmail.com>
2022-05-10 23:47:27 -0400

    Remove Loading component/User info states

--- fit_with_us/src/components/Loading.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Loading(props) {
-  return <h1>{props.message}</h1>;
-}
--- fit_with_us/src/components/SignUp.jsx
+++ fit_with_us/src/components/SignUp.jsx
@@ -19,18 +19,8 @@ export default function Form(props) {
   const [errorUsername, setErrorUsername] = useState(false);
   const [errorPassword, setErrorPassword] = useState(false);
   const [currentOptionsValue, setCurrentOptionsValues] = useState(["None"]);
-  const [userEmail, setUserEmail] = useState("");
-  const [userPassword, setUserPassword] = useState("");
-  const [userUsername, setUserUsername] = useState("");
 
-  useEffect(() => {
-    // const userInformation = getUserInformation();
-    // if (!errorEmail && !errorUsername && !errorPassword) {
-    //   console.log("Information can be used");
-    // } else {
-    //   console.log("Something is wrong");
-    // }
-  }, []);
+  useEffect(() => {}, []);
 
   //Variables
   const gender = [];
@@ -56,12 +46,6 @@ export default function Form(props) {
     }
     return false;
   };
-
-  // const checkEmail = async (params) => {
-  //   return await axios.get("http://localhost:8080/api/usernameCheck", {
-  //     params,
-  //   });
-  // };
 
   function addToOptionsList(e) {
     const numberOfOptions = currentDietaryRestrictions
@@ -101,205 +85,105 @@ export default function Form(props) {
     const gender = e.target[8].value;
     const dietaryRestrictions = currentOptionsValue;
 
-    await getUserInformation().then((response) => {
-      const userDatabase = response.data.users;
-      console.log(userDatabase);
-      const emailExists = checkForInformation(userDatabase, email, "email");
-      const usernameExists = checkForInformation(
-        userDatabase,
-        username,
-        "username"
-      );
-      const equalPasswords = checkEqualPasswords(
-        password,
-        passwordConfirmation
-      );
+    await getUserInformation()
+      .then(async (response) => {
+        const userDatabase = response.data.users;
+        console.log(userDatabase);
+        const emailExists = checkForInformation(userDatabase, email, "email");
+        const usernameExists = checkForInformation(
+          userDatabase,
+          username,
+          "username"
+        );
+        const equalPasswords = checkEqualPasswords(
+          password,
+          passwordConfirmation
+        );
 
-      console.log("Values", emailExists, usernameExists, equalPasswords);
+        console.log("Values", emailExists, usernameExists, equalPasswords);
 
-      if (emailExists) {
-        setErrorEmail("The email exists");
-      }
+        if (emailExists) {
+          setErrorEmail("The email exists");
+        }
 
-      if (usernameExists) {
-        setErrorUsername("The username exists");
-      }
+        if (usernameExists) {
+          setErrorUsername("The username exists");
+        }
 
-      if (!equalPasswords) {
-        setErrorPassword("The passwords do not match");
-      }
-    });
+        if (!equalPasswords) {
+          setErrorPassword("The passwords do not match");
+        }
 
-    // //Check Username
-    // Promise.all([
-    //   axios.get("http://localhost:8080/api/usernameCheck", { params }),
-    // ])
-    //   .then((all) => {
-    //     const userData = all[0].data.users;
-    //     console.log("Value", userData);
-    //     if (userData.length !== 0) {
-    //       throw setErrorUsername("The username already exists");
-    //     }
-    //   })
-    //   .then((all) => {
-    //     //Check Email
-    //     Promise.all([
-    //       axios.get("http://localhost:8080/api/emailCheck", { params }),
-    //     ]).then((all) => {
-    //       const userData = all[0].data.users;
-    //       if (userData.length !== 0) {
-    //         throw setErrorEmail("The email already exists");
-    //       }
-    //     });
-    //   })
-    //   .then((all) => {
-    //     //Makes entry into database
-    //     Promise.all([
-    //       axios.post("http://localhost:8080/api/user", {
-    //         email,
-    //         password,
-    //         username,
-    //         currentWeight,
-    //         goalWeight,
-    //         height,
-    //         age,
-    //       }),
-    //     ]).then((all) => {});
-    //   })
-    //   .then((all) => {
-    //     setTimeout(() => {
-    //       Promise.all([
-    //         axios.get("http://localhost:8080/api/user", { params }),
-    //         axios.get("http://localhost:8080/api/allUsers"),
-    //       ]).then((all) => {
-    //         //Returns user ID
-    //         const userData = all[0].data.users;
-    //         const user = userData[0];
-    //         const test = all[1].data;
-    //         console.log(test);
-    //         if (userData.length !== 0) {
-    //           props.loggedInUser(user.id);
-    //         }
-    //       });
-    //     }, 100);
-    //   })
-    //   .catch((err) => console.log(err.message));
-
-    // //Check Username
-    // Promise.all([
-    //   axios.get("http://localhost:8080/api/usernameCheck", { params }),
-    // ])
-    //   .then((all) => {
-    //     const userData = all[0].data.users;
-    //     console.log(userData);
-    //     if (userData.length !== 0) {
-    //       throw setErrorUsername("The username already exists");
-    //     }
-    //   })
-    //   .then((all) => {
-    //     //Check Email
-    //     Promise.all([
-    //       axios.get("http://localhost:8080/api/emailCheck", { params }),
-    //     ]).then((all) => {
-    //       const userData = all[0].data.users;
-    //       if (userData.length !== 0) {
-    //         throw setErrorEmail("The email already exists");
-    //       }
-    //     });
-    //   })
-    //   // .then(async (all) => {
-    //   //   //Makes entry into database
-    //   //   await axios.post("http://localhost:8080/api/user", {
-    //   //     email,
-    //   //     password,
-    //   //     username,
-    //   //     currentWeight,
-    //   //     goalWeight,
-    //   //     height,
-    //   //     age,
-    //   //     gender,
-    //   //     dietaryRestrictions,
-    //   //   });
-
-    //   //   const response = await axios.get("http://localhost:8080/api/user", {
-    //   //     params,
-    //   //   });
-    //   //   //Returns user ID
-    //   //   const userData = response[0].data.users;
-    //   //   const user = userData[0];
-    //   //   if (
-    //   //     userData.length !== 0 &&
-    //   //     !errorEmail &&
-    //   //     !errorUsername &&
-    //   //     !errorPassword
-    //   //   ) {
-    //   //     props.loggedInUser(user.id);
-    //   //   }
-    //   // })
-
-    //   .then((all) => {
-    //     Promise.all([
-    //       axios.get("http://localhost:8080/api/user", { params }),
-    //     ]).then((all) => {
-    //       //Returns user ID
-    //       const userData = all[0].data.users;
-    //       const user = userData[0];
-    //       if (
-    //         userData.length !== 0 &&
-    //         !errorEmail &&
-    //         !errorUsername &&
-    //         !errorPassword
-    //       ) {
-    //         props.loggedInUser(user.id);
-    //       }
-    //     });
-    //   })
-    //   .catch((err) => console.log(err.message));
+        if (emailExists || usernameExists || !equalPasswords) {
+          throw new Error("Information not correct");
+        }
+      })
+      .then(async (all) => {
+        //Makes entry into database
+        Promise.all([
+          axios.post("http://localhost:8080/api/user", {
+            email,
+            password,
+            username,
+            currentWeight,
+            goalWeight,
+            height,
+            age,
+          }),
+        ]).then((all) => {
+          console.log(all);
+        });
+      })
+      .then((all) => {
+        setTimeout(() => {
+          Promise.all([
+            axios.get("http://localhost:8080/api/user", { params }),
+            axios.get("http://localhost:8080/api/allUsers"),
+          ]).then((all) => {
+            //Returns user ID
+            const userData = all[0].data.users;
+            const user = userData[0];
+            const test = all[1].data;
+            console.log(test);
+            if (userData.length !== 0) {
+              props.loggedInUser(user.id);
+            }
+          });
+        }, 100);
+      });
   };
 
   return (
-    <>
-      <p>
-        {userEmail}
-        {userPassword}
-        {userUsername}
-      </p>
-
-      {userEmail && userPassword && userUsername ? (
-        <Loading message="Signing In" />
-      ) : (
-        <form
-          onSubmit={submitUserInformation}
-          action="http://localhost:8080/api/user"
-          method="POST"
-        >
-          <FormCategory name="email" type="email" />
-          {errorEmail ? <Error errorMessages={errorEmail} /> : null}
-          <FormCategory name="password" type="password" />
-          <FormCategory name="passwordConfirmation" type="password" />
-          {errorPassword ? <Error errorMessages={errorPassword} /> : null}
-          <FormCategory name="username" type="text" />
-          {errorUsername ? <Error errorMessages={errorUsername} /> : null}
-          <FormCategory name="currentWeight" type="number" />
-          <FormCategory name="goalWeight" type="number" />
-          <FormCategory name="height" type="number" />
-          <FormCategory name="age" type="number" />
-          <FormCategory
-            optionsName="gender-choices"
-            name="gender"
-            options={["-----", "Male", "Female", "Prefer not to disclose"]}
-          />
-          <FormCategory
-            onChange={addToOptionsList}
-            optionsName="dietary-choices"
-            name="dietaryRestrictions"
-            options={currentDietaryRestrictions}
-            size={true}
-            value={currentOptionsValue}
-          />
-          <Button name="Submit" />
-        </form>
-      )}
-    </>
+    <form
+      onSubmit={submitUserInformation}
+      action="http://localhost:8080/api/user"
+      method="POST"
+    >
+      <FormCategory name="email" type="email" />
+      {errorEmail ? <Error errorMessages={errorEmail} /> : null}
+      <FormCategory name="password" type="password" />
+      <FormCategory name="passwordConfirmation" type="password" />
+      {errorPassword ? <Error errorMessages={errorPassword} /> : null}
+      <FormCategory name="username" type="text" />
+      {errorUsername ? <Error errorMessages={errorUsername} /> : null}
+      <FormCategory name="currentWeight" type="number" />
+      <FormCategory name="goalWeight" type="number" />
+      <FormCategory name="height" type="number" />
+      <FormCategory name="age" type="number" />
+      <FormCategory
+        optionsName="gender-choices"
+        name="gender"
+        options={["-----", "Male", "Female", "Prefer not to disclose"]}
+      />
+      <FormCategory
+        onChange={addToOptionsList}
+        optionsName="dietary-choices"
+        name="dietaryRestrictions"
+        options={currentDietaryRestrictions}
+        size={true}
+        value={currentOptionsValue}
+      />
+      <Button name="Submit" />
+    </form>
   );
 }
