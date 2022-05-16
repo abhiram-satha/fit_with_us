5402b8e3cada42d4248c0c3a8815a1b550e55644
Brian Duong <brianminhduong@gmail.com>
2022-05-15 21:20:16 -0400

    Create UserProfile and Badge components

---
+++ fit_with_us/src/components/Badge.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Badge(props) {
+  return <image className="image is-32x32" src={props.img_url} />;
+}
---
+++ fit_with_us/src/components/UserProfile.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import axios from "axios";
+import Badge from "./Badge";
+
+export default function UserProfile(props) {}
