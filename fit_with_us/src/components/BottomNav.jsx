5c28dc7e56dc2b9230fadf2d41d84fac2964d835
Lucas Lee <lee.lucas.hyunguk@gmail.com>
2022-05-16 21:38:22 -0700

    turn bottomnav color switch logic into state to improve latency

--- fit_with_us/src/components/BottomNav.jsx
+++ fit_with_us/src/components/BottomNav.jsx
@@ -1,19 +1,49 @@
 import React from "react";
+import { useState } from "react";
 
 export default function BottomNav(props) {
 
+  const [youClass, setYouClass] = useState(window.location.href === "http://localhost:3003/homepage" ? "is-active" : "");
+  const [usClass, setUsClass] = useState(window.location.href === "http://localhost:3003/posts" ? "is-active" : "");
+
+  const handleYouClick = () => {
+    setUsClass("");
+    setYouClass("is-active");
+  }
+
+  const handleUsClick = () => {
+    setYouClass("");
+    setUsClass("is-active");
+  }
+
+  // return (
+  //   <div class="navbar tabs is-toggle is-centered is-medium is-fullwidth is-fixed-bottom mb-0">
+  //   <ul>
+  //     <li class={window.location.href === "http://localhost:3003/posts" ? "" : "is-active"} id="you">
+  //       {/* <Link to="/homepage">You</Link> */}
+  //       <a href="/homepage">You</a>
+  //     </li>
+  //     <li class={window.location.href === "http://localhost:3003/posts" ? "is-active" : ""} id="us">
+  //       {/* <Link to="/posts">Us</Link> */}
+  //       <a href="/posts">Us</a>
+  //     </li>
+  //   </ul>
+  // </div> 
+  // );
+
   return (
-    <div class="navbar tabs is-toggle is-centered is-medium is-fullwidth is-fixed-bottom mb-0">
+        <div class="navbar tabs is-toggle is-centered is-medium is-fullwidth is-fixed-bottom mb-0">
     <ul>
-      <li class={window.location.href === "http://localhost:3003/posts" ? "" : "is-active"} id="you">
+      <li class={youClass} onClick={handleYouClick} id="you">
         {/* <Link to="/homepage">You</Link> */}
         <a href="/homepage">You</a>
       </li>
-      <li class={window.location.href === "http://localhost:3003/posts" ? "is-active" : ""} id="us">
+      <li class={usClass} onClick={handleUsClick} id="us">
         {/* <Link to="/posts">Us</Link> */}
         <a href="/posts">Us</a>
       </li>
     </ul>
   </div> 
   );
+
 }
