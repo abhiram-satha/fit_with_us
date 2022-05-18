d0a4422c329ce98abacd543fb72f8737d99db164
Brian Duong <brianminhduong@gmail.com>
2022-05-17 23:06:34 -0400

    Add description column to badges table

--- fit_with_us/src/components/TopNav.jsx
+++ fit_with_us/src/components/TopNav.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 import "../helpers/navbarQuery";
 import { Link, Route, Routes } from "react-router-dom";
 import UserProfile from "./UserProfile";
 
 export default function TopNav(props) {
+  const [burgerClass, setBurgerClass] = useState("navbar-burger");
+  const [menuClass, setMenuClass] = useState("navbar-menu");
+  const handleBurger = () => {
+    if (burgerClass === "navbar-burger") {
+      setBurgerClass("navbar-burger is-active");
+      setMenuClass("navbar-menu is-active");
+    } else {
+      setBurgerClass("navbar-burger");
+      setMenuClass("navbar-menu");
+    }
+  };
   return (
     <>
       <Routes>
@@ -20,22 +31,19 @@ export default function TopNav(props) {
         />
       </Routes>
       <nav
-        className="navbar is-fixed-top"
+        class="navbar is-fixed-top"
         role="navigation"
         aria-label="main navigation"
       >
-        <div className="navbar-brand">
-          <a className="navbar-item" href="https://bulma.io">
-            <img
-              src="https://bulma.io/images/bulma-logo.png"
-              width="112"
-              height="28"
-            />
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://bulma.io">
+            <img src="https://i.imgur.com/cBZaCDn.png" />
           </a>
 
           <button
+            onClick={handleBurger}
             role="button"
-            className="navbar-burger"
+            class={burgerClass}
             aria-label="menu"
             aria-expanded="false"
             data-target="navbarBasicExample"
@@ -46,28 +54,28 @@ export default function TopNav(props) {
           </button>
         </div>
 
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <a className="navbar-item">
+        <div id="navbarBasicExample" class={menuClass}>
+          <div class="navbar-start">
+            <a class="navbar-item">
               <Link to="/profile">Profile</Link>
             </a>
 
-            <a className="navbar-item">Messages</a>
+            <a class="navbar-item">Messages</a>
 
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">Update your journey</a>
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">Update your journey</a>
 
-              <div className="navbar-dropdown">
-                <a className="navbar-item">Update current weight</a>
-                <a className="navbar-item">Change recipe</a>
+              <div class="navbar-dropdown">
+                <a class="navbar-item">Update current weight</a>
+                <a class="navbar-item">Change recipe</a>
               </div>
             </div>
-            <a className="navbar-item">Settings</a>
+            <a class="navbar-item">Settings</a>
           </div>
 
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="buttons">
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
                 {props.backButton ? (
                   <Button
                     onClick={props.backButton}
