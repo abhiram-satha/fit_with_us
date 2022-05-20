fda80b8d887e5587678b6e21cae79f9cef950d3f
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-05-20 23:55:11 +0000

    Changes to the placeholder text

--- fit_with_us/src/components/UpdateWeightGoal.jsx
+++ fit_with_us/src/components/UpdateWeightGoal.jsx
@@ -3,11 +3,11 @@ import Button from "./Button"
 export default function UpdateWeightGoal ({users, updateGoalWeight}) {
 // console.log(users)
   const {goal_weight} = users.users[0]
-
+  const placeholderString = `Current Goal Weight: ${goal_weight}`
   return (
     <>
       <form onSubmit={(event)=>handleUpdatedWeightGoal(event)}>
-        <input className="input is-primary" type="number" placeholder={goal_weight}/>
+        <input className="input is-primary" type="number" placeholder={placeholderString}/>
         <input type="submit" name="Update Goal Weight"></input>
         {/* <Button  type="submit" name="Update Goal Weight"/> */}
       </form>
